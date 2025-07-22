725f50658dc2f9fb1e262cb7fd3902512be12f9d
roykamsi <kamsi.rino@gmail.com>
2025-07-22 15:34:03 +0200

    Inserted the form

--- src/app/email-form/email-form.component.ts
+++ src/app/email-form/email-form.component.ts
@@ -1,14 +1,71 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-email-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './email-form.component.html',
   styleUrl: './email-form.component.scss'
 })
 export class EmailFormComponent {
+  formData = {
+    name: '',
+    email: '',
+    subject: '',
+    message: ''
+  };
 
+  isSubmitting = false;
+  submitMessage = '';
+  submitSuccess = false;
 
+  onSubmit() {
+    if (this.isSubmitting) return;
 
+    this.isSubmitting = true;
+    this.submitMessage = '';
+
+    // Create FormData to send to Formspree
+    const formData = new FormData();
+    formData.append('name', this.formData.name);
+    formData.append('email', this.formData.email);
+    formData.append('subject', this.formData.subject);
+    formData.append('message', this.formData.message);
+
+    // Send to Formspree
+    fetch('https://formspree.io/f/mdkdeaow', {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      if (response.ok) {
+        this.submitSuccess = true;
+        this.submitMessage = 'The message has been sent successfully. I\'ll get in touch with you soon!';
+        this.resetForm();
+      } else {
+        throw new Error('Failed to send message');
+      }
+    })
+    .catch(error => {
+      this.submitSuccess = false;
+      this.submitMessage = 'An error occurred while sending the message. Please try again later.';
+    })
+    .finally(() => {
+      this.isSubmitting = false;
+    });
+  }
+
+  private resetForm() {
+    this.formData = {
+      name: '',
+      email: '',
+      subject: '',
+      message: ''
+    };
+  }
 }
