88252c8d39e5a99cc6ff74fa61161bc762c7d098
roykamsi <kamsi.rino@gmail.com>
2025-07-29 17:56:31 +0200

    Corretto il percorso di importazione per l'ambiente in map.component.ts e aggiornato il percorso in tsconfig.json

--- src/app/components/email-form/fragments/map/map.component.ts
+++ src/app/components/email-form/fragments/map/map.component.ts
@@ -1,6 +1,6 @@
 import { PLATFORM_ID, inject, Component, AfterViewInit, OnDestroy } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
-import { environment } from '@environments/environment';
+import { environment } from './../../../../../environments/environment';
 
 @Component({
   selector: 'app-map',
